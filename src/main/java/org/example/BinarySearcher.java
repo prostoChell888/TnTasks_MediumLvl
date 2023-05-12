48a4c5f67b630041b3bad55dd491c1cf3f441d83
Александр Бахмутов <alexdmitralex@gmail.com>
2023-05-12 10:17:15 +0300

    init commit

---
+++ src/main/java/org/example/BinarySearcher.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class BinarySearcher {
+
+    public static void search(){
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
