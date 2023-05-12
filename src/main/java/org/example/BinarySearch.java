22a457085502ea09def3b2bb1dd63ad6943a453a
Александр Бахмутов <alexdmitralex@gmail.com>
2023-05-12 10:59:16 +0300

    add fun search

--- src/main/java/org/example/BinarySearch.java
+++ src/main/java/org/example/BinarySearch.java
@@ -1,7 +1,33 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class BinarySearch {
 
+    public static <T extends Comparable<T>> int search(List<T> list, T target) {
+        int leftOrder = 0;
+        int rightOrder = list.size() - 1;
+
+        while (leftOrder <= rightOrder) {
+            int mid = (leftOrder + rightOrder) >> 1;
+
+            int resOfComp = target.compareTo(list.get(mid));
+
+            if (resOfComp == 0) return mid;
+            else if (resOfComp > 0) leftOrder = mid + 1;
+            else rightOrder = mid - 1;
+        }
+
+        return -1;
+    }
 
 
+    public static void main(String[] args) {
+
+        List<Integer> list = List.of(10, 20, 30);
+
+        System.out.println(search(list, 20));
+    }
 }
