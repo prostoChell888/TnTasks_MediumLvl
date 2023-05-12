be153873beb73deea102d4f7d0adb95af65f01eb
Александр Бахмутов <alexdmitralex@gmail.com>
2023-05-12 12:04:12 +0300

    add comments

--- src/main/java/org/example/BinarySearch.java
+++ src/main/java/org/example/BinarySearch.java
@@ -4,8 +4,22 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * @author prostoChell888
+ * @version 1.0
+ * This class provides a method for sorting an array
+ */
 public class BinarySearch {
 
+
+    /**
+     * This method implements binary search in a sorted array
+     * @param list sorted array
+     * @param target the element whose position you need to find
+     *               or -1 if there is no such element
+     * @return the position of the desired element
+     * @param <T> a class that implements the Comparable interface
+     */
     public static <T extends Comparable<T>> int search(List<T> list, T target) {
         int leftOrder = 0;
         int rightOrder = list.size() - 1;
