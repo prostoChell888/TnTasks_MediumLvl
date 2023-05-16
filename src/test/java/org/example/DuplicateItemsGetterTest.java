0774edb585e545d63c9dbff6f804383c9d16f579
Александр Бахмутов <alexdmitralex@gmail.com>
2023-05-16 18:16:14 +0300

    add tests

---
+++ src/test/java/org/example/DuplicateItemsGetterTest.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DuplicateItemsGetterTest {
+
+
+
+    @ParameterizedTest
+    @MethodSource("testCases")
+    public <T> void getDuplicates_List_ListWithDuplicates(List<T> input, List<T> expectedOutput) {
+        List<T> actualOutput = DuplicateItemsGetter.getDuplicates(input);
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+
+    private static List<Object[]> testCases() {
+        return Arrays.asList(
+                new Object[]{List.of(1, 2, 3, 2, 3, 4, 4), List.of(2, 3, 4)},
+                new Object[]{List.of("apple", "banana", "apple", "cherry"), List.of("apple")},
+                new Object[]{List.of(1, 2, 3, 4, 5), new ArrayList<>()},
+                new Object[]{List.of("a", "b", "c", "b", "c", "d"), List.of("b", "c")}
+        );
+    }
+
+
+    @Test
+    public <T> void getDuplicates_EmptyList_EmptyList() {
+        List<T> input = new ArrayList<>();
+        List<T> expectedOutput = new ArrayList<>();
+        List<T> actualOutput = DuplicateItemsGetter.getDuplicates(input);
+        assertEquals(expectedOutput, actualOutput);
+    }
+}
