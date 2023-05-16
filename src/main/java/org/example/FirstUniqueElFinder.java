d0a3e6b0c16ac817b11f4f86662cf890c498a47a
Александр Бахмутов <alexdmitralex@gmail.com>
2023-05-16 17:07:36 +0300

    add fun

--- src/main/java/org/example/FirstUniqueElFinder.java
+++ src/main/java/org/example/FirstUniqueElFinder.java
@@ -3,6 +3,22 @@ package org.example;
 public class FirstUniqueElFinder {
 
 
+    public static char find(String str) {
+        var arr = str.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            boolean isUnique = true;
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] == arr[j] && i != j ) {
+                    isUnique = false;
+                    break;
+                }
+            }
+            if (isUnique) return arr[i];
+        }
+        return '\0';
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
