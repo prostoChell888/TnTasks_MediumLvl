10fd937890970aed616c21fcbca9714f3129a32b
Александр Бахмутов <alexdmitralex@gmail.com>
2023-05-16 18:16:06 +0300

    add comments

--- src/main/java/org/example/DuplicateItemsGetter.java
+++ src/main/java/org/example/DuplicateItemsGetter.java
@@ -5,8 +5,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
+/**
+ * return list of duplicate items
+ */
 public class DuplicateItemsGetter {
 
+    /**
+     * @param list of anu Elements
+     * @return list of duplicate items
+     */
     public static <T> List<T> getDuplicates(List<T> list) {
         Map<T, Integer> countMap = new HashMap<>();
 
