d38436d460d8585cbea8f8c0c55d5bf3c8518534
Александр Бахмутов <alexdmitralex@gmail.com>
2023-05-18 16:01:08 +0300

    add Node

---
+++ src/main/java/org/example/Node.java
@@ -0,0 +1,11 @@
+package org.example;
+
+class Node {
+    private Node left;
+    private Node right;
+    private final int value;
+
+    public Node(int value) {
+        this.value = value;
+    }
+}
