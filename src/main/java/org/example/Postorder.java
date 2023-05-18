b530f254278b30eff257fbd8eba09bbab377b7c5
Александр Бахмутов <alexdmitralex@gmail.com>
2023-05-18 16:41:53 +0300

    add postorder traversal of the tree fun

--- src/main/java/org/example/Postorder.java
+++ src/main/java/org/example/Postorder.java
@@ -1,8 +1,34 @@
 package org.example;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Postorder {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    /**
+     * Implements a postorder traversal of the tree in depth
+     * @param root node
+     * @return list with tree int values
+     */
+    public static List<Integer> getListFomTree(Node root) {
+        List<Integer> listOfInt = new LinkedList<>();
+        if (root == null) return listOfInt;
+
+        preorderTraversal(listOfInt, root.getLeft());
+        preorderTraversal(listOfInt, root.getRight());
+
+        listOfInt.add(root.getValue());
+
+        return listOfInt;
+    }
+
+
+    private static void preorderTraversal(List<Integer> listOfInt, Node node) {
+        if (node == null) return;
+
+        preorderTraversal(listOfInt, node.getLeft());
+        preorderTraversal(listOfInt, node.getRight());
+
+        listOfInt.add(node.getValue());
     }
 }
