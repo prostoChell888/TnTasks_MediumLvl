9c4cf40a7f19fce24dfdb2f629621a1b0e17371f
Александр Бахмутов <alexdmitralex@gmail.com>
2023-05-26 09:02:47 +0300

    replaced recursion with a loop

--- src/main/java/org/example/Postorder.java
+++ src/main/java/org/example/Postorder.java
@@ -1,25 +1,45 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class Postorder {
 
     /**
      * Implements a postorder traversal of the tree in depth
+     *
      * @param root node
      * @return list with tree int values
      */
     public static List<Integer> getListFomTree(Node root) {
-        List<Integer> listOfInt = new LinkedList<>();
-        if (root == null) return listOfInt;
+        List<Integer> result = new ArrayList<>();
 
-        preorderTraversal(listOfInt, root.getLeft());
-        preorderTraversal(listOfInt, root.getRight());
+        if (root == null) return result;
 
-        listOfInt.add(root.getValue());
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
 
-        return listOfInt;
+        Node prev = null;
+        while (!stack.isEmpty()) {
+            Node current = stack.peek();
+            if (current.getLeft() == null && current.getRight() == null ||
+                    (prev != null && (prev == current.getLeft() || prev == current.getRight()))) {
+                result.add(current.getValue());
+                stack.pop();
+                prev = current;
+            } else {
+                if (current.getRight() != null) {
+                    stack.push(current.getRight());
+                }
+                if (current.getLeft() != null) {
+                    stack.push(current.getLeft());
+                }
+            }
+        }
+
+        return result;
     }
 
 
