38934dcea7314334d8adec08de326a47d52bb391
Александр Бахмутов <alexdmitralex@gmail.com>
2023-06-04 15:14:37 +0300

    add source code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,20 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
+    private static final int MIN_AGE = 14;
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            persons.add(PersonCreator.createPerson(10));
+        }
+
+        var peopleWithAgeMoreThanMinAge = PersonFilter.filterByMinAge(persons, MIN_AGE);
+        peopleWithAgeMoreThanMinAge.forEach(person -> System.out.println(person.getAddress()));
+
     }
 }
