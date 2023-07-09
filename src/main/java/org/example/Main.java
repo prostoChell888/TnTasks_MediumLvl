ccf0ad916e48c9ad97202b4665f1893ac4f206d1
Александр Бахмутов <alexdmitralex@gmail.com>
2023-07-09 15:53:33 +0300

    add main

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.setLeft(new Node(2));
+        root.setRight(new Node(3));
+        root.getLeft().setLeft(new Node(4));
+        root.getLeft().setRight(new Node(5));
+
+        List<Integer> preorderList = Preorder.getListFomTree(root);
+        System.out.println("Preorder traversal result: " + preorderList);
+    }
+}
